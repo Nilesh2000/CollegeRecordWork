9739804436fade125eca8a3b0348f94101529e90
Nilesh D <nileshlund@gmail.com>
2020-09-23 19:44:33 +0530

    Completed Exercise 2b

---
+++ Semester 5/Data Communication and Networking/Ex2b/DateServer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class DateServer {
+  public static void main(String args[]) throws Exception {
+    ServerSocket s = new ServerSocket(5217);
+    while(true) {
+      System.out.println("Waiting For Connection...");
+      Socket soc = s.accept();
+      DataOutputStream out = new DataOutputStream(soc.getOutputStream());
+      out.writeBytes("Date With Time : " + (new Date()).toString() + "\n");
+
+      out.close();
+      soc.close();
+    }
+  }
+}
