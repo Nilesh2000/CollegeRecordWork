dfd93340866166fc9de62670b9b1803dec490517
Nilesh D <nileshlund@gmail.com>
2020-09-25 21:45:26 +0530

    Completed Ex5b

---
+++ Semester 5/Data Communication and Networking/Ex5b/Traceroute.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+
+class Traceroute {
+  public static void main(String args[]) {
+  BufferedReader in;
+    try {
+      Runtime r = Runtime.getRuntime();
+      Process p = r.exec("tracert www.youtube.com");
+      in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line;
+
+      if(p == null) {
+        System.out.println("Could not connect");
+      }
+
+      while((line=in.readLine()) != null) {
+          System.out.println(line);
+      }
+
+    } catch(IOException e) {
+      System.out.println(e.toString());
+    }
+  }
+}
